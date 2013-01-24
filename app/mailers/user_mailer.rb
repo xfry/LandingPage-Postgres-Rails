45d4b17ace10695b9490a2596be10ebb578be429
xfry <fry.andrade@gmail.com>
2013-01-24 11:35:16 -0500

    new

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ActionMailer::Base
+  default from: "team@alertab.com"
+  def welcome_message(user)
+  	@user = user
+  	@url = "http://gmail.com/login"
+  	mail(
+  		to: user.email,
+  		subject: "Bienviado #{@user.name}",
+  		#body: "Hola  #{@user.name} Muchas gracias por interesarte en conocer mas de Alertab"
+  		template_path: 'user_mailer',
+  		template_name: 'welcome_message'
+  		)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Landingpage::Application.config.secret_token = '0f7c523efd530c6b826439aeda45b1a7cfc40e45c6df892ef34dfeeb508f4643c553d0154233ff9402b820fd8a5a62c3a59fc8d17e96c30031f95043dea2c51a'
