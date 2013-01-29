9160eaf5ca4628072b9b82789f5181518d2d6e6d
xfry <fry.andrade@gmail.com>
2013-01-28 19:07:00 -0500

    LandingPage Alertab

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
   	@url = "http://gmail.com/login"
   	mail(
   		to: user.email,
-  		subject: "Bienviado #{@user.name}",
+  		subject: "Bienvenido, #{@user.name}",
   		#body: "Hola  #{@user.name} Muchas gracias por interesarte en conocer mas de Alertab"
   		template_path: 'user_mailer',
   		template_name: 'welcome_message'
