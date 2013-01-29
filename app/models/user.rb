e5d548b6a2e0e98c838ada0711a7ac47b7feaac8
xfry <fry.andrade@gmail.com>
2013-01-29 11:18:35 -0500

    Nuevos cambios modelo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
-  validates :name, :email, :presence => true, :uniqueness => true;
+  validates :email, :presence => true, :uniqueness => true;
 end
