3f8c37afde2f2aa243852fba097e1df65eafa95a
xfry <fry.andrade@gmail.com>
2013-01-31 10:45:51 -0500

    cambio en el index y mensaje principal de alertab

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Landingpage::Application.routes.draw do
-  resources :users
-
+  resources :users, :except => [:index]
+  resources :users, :only => [:new]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,5 +57,6 @@ Landingpage::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
+  match 'us3rsl1s720134ler74b', :to => 'users#index'
   # match ':controller(/:action(/:id))(.:format)'
 end
